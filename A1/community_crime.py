b4d32d67f7dbf841a49ce02cf0ecc2c5f4234b47
John Wu <john.wu@mail.mcgill.ca>
2018-01-24 01:23:53 -0500

    Midway through part 3

---
+++ A1/community_crime.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+DATA_DIR = 'community-crime/'
+STEP_SIZE = 1e-5
+
+def prepareData():
+    # clean data
+    df = pd.read_csv('{}raw_data.csv'.format(DATA_DIR), header=None)
+    df.drop([0, 1, 2, 3, 4], axis=1, inplace=True)
+    df = df.replace('?', np.NaN).astype(np.float64)
+    df.fillna(df.mean(), inplace=True)
+    df.to_csv('{}cleaned_data.csv'.format(DATA_DIR), index=False, header=False)
+    # split data
+    for i in range(1, 6):
+        msk = np.random.rand(len(df)) < 0.8
+        train = df[msk]
+        test = df[~msk]
+        train.to_csv('{}CandC−train{}.csv'.format(DATA_DIR, i), index=False, header=False)
+        test.to_csv('{}CandC−test{}.csv'.format(DATA_DIR, i), index=False, header=False)
+
+def createComputationMatrices(train_data, test_data):
+    # create vector
+    # train_data_output = train_data[train_data.shape[1] - 1]
+    train_data_output = pd.DataFrame([train_data[train_data.shape[1] - 1]])
+    test_data_output = pd.DataFrame([test_data[test_data.shape[1] - 1]])
+    # modify input matrix (shift and add column of ones)
+    for i in reversed(range(1, train_data.shape[1])):
+        train_data[i] = train_data[i - 1]
+    train_data.drop([0], axis=1, inplace=True)
+    train_data.insert(0, 0, np.ones(train_data.shape[0]))
+    for i in reversed(range(1, test_data.shape[1])):
+        test_data[i] = test_data[i - 1]
+    test_data.drop([0], axis=1, inplace=True)
+    test_data.insert(0, 0, np.ones(test_data.shape[0]))
+    # create w vector
+    w = pd.DataFrame(np.random.randint(low=0, high=10, size=(1, train_data.shape[1])))
+    return train_data, train_data_output, test_data, test_data_output, w
+
+def calculateMSE(w, test_data_input, test_data_output):
+    predicted_output = np.matmul(w, test_data_input.T)
+    squared_error = np.power(np.subtract(predicted_output, test_data_output), 2)
+    return np.sum(np.sum(squared_error)) / squared_error.size
+
+def performRegression():
+    train_data = pd.read_csv('{}CandC−train{}.csv'.format(DATA_DIR, 1), header=None)
+    test_data = pd.read_csv('{}CandC−test{}.csv'.format(DATA_DIR, 1), header=None)
+
+    train_data_input, train_data_output, test_data_input, test_data_output, w = createComputationMatrices(train_data, test_data)
+
+    for i in range(10000):
+        predicted_output = np.matmul(w, train_data_input.T)
+        loss = np.matmul(np.subtract(predicted_output, train_data_output), train_data_input) / train_data_output.shape[0]
+        w = w - STEP_SIZE * loss
+
+    mse = calculateMSE(w, test_data_input, test_data_output)
+    print(mse)
+
+
+def main():
+    prepareData()
+    performRegression()
+
+main()
--- A1/gradient_descent.py
+++ A1/gradient_descent.py
@@ -8,6 +8,7 @@ VALID_DATA = 'data/Dataset_2_valid.csv'
 TEST_DATA = 'data/Dataset_2_test.csv'
 
 ERROR_THRESHOLD = 0.1
+EPOCH_CYCLES = 10000
 
 ''' Parses the data file into a data frame object '''
 def parseData(file):
@@ -18,16 +19,6 @@ def parseData(file):
 ''' Computes the output of a given input using indicated parameters '''
 def computeOutput(w0, w1, x):
     return w0 + w1 * x
-
-''' Performs one full epoche of the data '''
-def performEpoche(train_data, w0, w1, step_size):
-    for i in range(train_data.shape[0]):
-        predicted_output = computeOutput(w0, w1, train_data[0][i])
-        actual_output = train_data[1][i]
-        loss = predicted_output - actual_output
-        w0 = w0 - step_size * loss
-        w1 = w1 - step_size * loss * train_data[0][i]
-    return w0, w1
 
 ''' Compute the mean squared error '''
 def computeMSE(w0, w1, test_data):
@@ -48,21 +39,76 @@ def visualizeLearning(iteration_values, mse_values, title):
     plt.tight_layout()
     plt.show()
 
-''' Part 1 '''
-train_data = parseData(TRAIN_DATA)
-valid_data = parseData(VALID_DATA)
-step_size = 1e-6
-w0, w1 = random.randint(0, 10), random.randint(0, 10)
-print('Initial w0: ', w0, ', Initial w1: ', w1)
-iteration_values, mse_train_values, mse_valid_values = [], [], []
-for i in range(1, 10000):
-    w0, w1 = performEpoche(train_data, w0, w1, step_size)
-    mse_train_values.append(computeMSE(w0, w1, train_data))
-    mse_valid_values.append(computeMSE(w0, w1, valid_data))
-    iteration_values.append(i)
-visualizeLearning(iteration_values, mse_train_values, 'Learning Curve (Training Data)')
-visualizeLearning(iteration_values, mse_valid_values, 'Learning Curve (Validation Data)')
-print('Final w0: ', w0, ', Final w1: ', w1)
-print('MSE (Training Data): ', mse_train_values[10000-1])
-print('MSE (Validation Data): ', mse_valid_values[10000-1])
+def visualizeStepSizes(step_sizes, mse_values):
+    plt.scatter(step_sizes, mse_values)
+    plt.plot(step_sizes, mse_values)
+    plt.xscale('log')
+    plt.xticks(step_sizes)
+    plt.title('Step Sizes vs MSE')
+    plt.xlabel('Step Size')
+    plt.ylabel('Mean Squared Error')
+    plt.tight_layout()
+    plt.show()
 
+def visualizeEpoch(w_values, test_data):
+    interval_length = [1, int(round(EPOCH_CYCLES/1000)), int(round(EPOCH_CYCLES/100)), int(round(EPOCH_CYCLES/10)), EPOCH_CYCLES-2]
+    for i in range(5):
+        plt.scatter(test_data[0], test_data[1])
+        plt.plot(test_data[0],  w_values[interval_length[i]][0] + w_values[interval_length[i]][1] * test_data[0], 'r--')
+        plt.title('Epoch Visualization')
+        plt.xlabel('Input')
+        plt.ylabel('Output')
+        plt.tight_layout()
+        plt.show()
+
+
+def performLinearRegression(train_data, valid_data, step_size, w0, w1):
+    iteration_values, mse_train_values, mse_valid_values, w_values = [], [], [], []
+    for i in range(1, EPOCH_CYCLES):
+        for j in range(train_data.shape[0]):
+            predicted_output = computeOutput(w0, w1, train_data[0][j])
+            actual_output = train_data[1][j]
+            loss = predicted_output - actual_output
+            w0 = w0 - step_size * loss
+            w1 = w1 - step_size * loss * train_data[0][j]
+        mse_train_values.append(computeMSE(w0, w1, train_data))
+        mse_valid_values.append(computeMSE(w0, w1, valid_data))
+        w_values.append([w0,w1])
+        iteration_values.append(i)
+    print('Final w0: ', w0, ', Final w1: ', w1)
+    return iteration_values, mse_train_values, mse_valid_values, w_values, mse_train_values[EPOCH_CYCLES - 2], mse_valid_values[EPOCH_CYCLES - 2]
+
+def main():
+    train_data = parseData(TRAIN_DATA)
+    valid_data = parseData(VALID_DATA)
+    test_data = parseData(TEST_DATA)
+    w0, w1 = random.randint(0, 10), random.randint(0, 10)
+    print('Initial w0: ', w0, ', Initial w1: ', w1)
+
+    # ''' Part 1 '''
+    # iteration_values, mse_train_values, mse_valid_values, w_values, best_mse_train, best_mse_valid = performLinearRegression(train_data, valid_data, 1e-6, w0, w1)
+    # visualizeLearning(iteration_values, mse_train_values, 'Learning Curve (Training Data)')
+    # visualizeLearning(iteration_values, mse_valid_values, 'Learning Curve (Validation Data)')
+    # print('MSE (Training Data): ', best_mse_train)
+    # print('MSE (Validation Data): ', best_mse_valid)
+    # print(w_values)
+    #
+    # ''' Part 2'''
+    # step_sizes, mse_values = [], []
+    # for i in range(1, 10):
+    #     step_size = 1 * 10**(-i)
+    #     print(step_size)
+    #     iteration_values, mse_train_values, mse_valid_values, w_values, best_mse_train, best_mse_valid = performLinearRegression(train_data, valid_data, step_size, w0, w1)
+    #     step_sizes.append(step_size)
+    #     mse_values.append(best_mse_valid)
+    # visualizeStepSizes(step_sizes, mse_values)
+
+    ''' Part 3 '''
+    #optimal_step_size = step_sizes[mse_values.index(min(mse_values))]
+    optimal_step_size = 1e-4
+    iteration_values, mse_train_values, mse_valid_values, w_values, best_mse_train, best_mse_valid = performLinearRegression(train_data, valid_data, optimal_step_size, w0, w1)
+    print(w_values)
+    visualizeEpoch(w_values, test_data)
+
+
+main()
